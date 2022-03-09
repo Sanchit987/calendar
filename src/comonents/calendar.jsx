f0d111ff8b22e33d4ed7868c931c83e209e5fa98
Sanchit987 <kherasanchit1@gmail.com>
2022-03-09 16:00:07 +0530

    Added both dark and light modes

--- src/comonents/calendar.jsx
+++ src/comonents/calendar.jsx
@@ -13,37 +13,39 @@ const Calendar = () => {
 
     
     return ( <Fragment>
-        <div className="container container-light">
-            <div className="heading-flex">
-                <button className={`button ${toggled?"button-dark":"button-light"}`} onClick={getPrevMonth}>{"<"}</button>
-                    <p className="heading">{`${monthNames[selectedDate.getMonth()]} - ${selectedDate.getFullYear()}`}</p>
-                <button className={`button ${toggled?"button-dark":"button-light"}`} onClick={getNextMonth}>{">"}</button>
+        <div className="parent">
+        <Toggle onChange={(e) => setToggled(e.target.checked)}/>    
+            <div className={`container ${toggled?'container-dark':'container-light'}`} >
+                <div className="heading-flex">
+                    <button className={`button ${toggled?'button-dark':'button-light'}`} onClick={getPrevMonth}>{"<"}</button>
+                        <p className={`heading ${toggled?'heading-dark':'heading-light'}`}>{`${monthNames[selectedDate.getMonth()]} - ${selectedDate.getFullYear()}`}</p>
+                    <button className={`button ${toggled?'button-dark':'button-light'}`} onClick={getNextMonth}>{">"}</button>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            {daysShort.map(day => (
+                                <th className={`${toggled?'month-dark':'month-light'}`} key={day}>{day}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Object.values(calendarRows).map(cols => {
+                                return <tr key = {cols[0].date}>
+                                    {cols.map(col => (
+                                        col.date === todayFormatted ? <td key={col.date} className={`${col.classes} today`} onClick={()=>dateClickHandler(col.date)}>
+                                            {col.value}
+                                        </td>
+                                        : <td key={col.date} className={`${toggled?(col.classes==='in-prev-month' || col.classes==='in-next-month')?col.classes + " next-prev-dark":col.classes+" current-month-dark":col.classes}`} onClick={() => {dateClickHandler(col.date)}}>{col.value}</td>
+                                    ))}
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        {daysShort.map(day => (
-                            <th key={day}>{day}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        Object.values(calendarRows).map(cols => {
-                            return <tr key = {cols[0].date}>
-                                {cols.map(col => (
-                                    col.date === todayFormatted ? <td key={col.date} className={`${col.classes} today`} onClick={()=>dateClickHandler(col.date)}>
-                                        {col.value}
-                                    </td>
-                                    : <td key={col.date} className={col.classes} onClick={() => {dateClickHandler(col.date)}}>{col.value}</td>
-                                ))}
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </table>
         </div>
-        <Toggle onChange={(e) => setToggled(e.target.checked)}/>
     </Fragment> );
 }
  
